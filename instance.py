b3818363ce99ce173dbb0c23ad2229efd1ddf566
Igor Martayan <igor@martayan.org>
2023-01-03 10:12:52 +0100

    Mark private methods, separate fetch_users and build_graph

--- instance.py
+++ instance.py
@@ -5,16 +5,15 @@ import json
 
 
 class Instance:
-    def __init__(self, domain, build_graph=False, debug=False):
+    def __init__(self, domain, debug=False):
         self.domain = domain
-        self.base_url = "https://" + domain
-        self.build_graph = build_graph
+        self._base_url = "https://" + domain
         self.debug = debug
         self.user_count = None
         self.users = {}
-        self.session = None
-        self.handle_session = False
-        self.throttler = Throttler(
+        self._session = None
+        self._handle_session = False
+        self._throttler = Throttler(
             rate_limit=300, period=300, retry_interval=1
         )  # https://docs.joinmastodon.org/api/rate-limits/
 
@@ -35,54 +34,54 @@ class Instance:
             }
             json.dump(data, file)
 
-    def endpoint(self, endpoint):
-        if self.session is None or self.session._base_url is None:
-            return self.base_url + endpoint
+    def _endpoint(self, endpoint):
+        if self._session is None or self._session._base_url is None:
+            return self._base_url + endpoint
         return endpoint
 
-    def init_session(self):
-        if self.session is None:
-            self.session = aiohttp.ClientSession(base_url=self.base_url)
-            self.handle_session = True
+    def _init_session(self):
+        if self._session is None:
+            self._session = aiohttp.ClientSession(base_url=self._base_url)
+            self._handle_session = True
 
-    async def close_session(self):
-        if self.handle_session:
-            await self.session.close()
-            self.session = None
+    async def _close_session(self):
+        if self._handle_session:
+            await self._session.close()
+            self._session = None
 
-    def handle_error(self, response):
-        print(f"Error {response.status} {response.reason} ({response.url})")
+    def _handle_error(self, response):
+        print(f"({response.url}) error {response.status}")
         response.raise_for_status()
 
-    async def get_info(self):
+    async def _get_info(self):
         """
         https://docs.joinmastodon.org/methods/instance/#v1
         """
         if self.debug:
             print(f"({self.domain}) get info")
-        url = self.endpoint("/api/v1/instance")
-        async with self.throttler:
-            async with self.session.get(url=url) as response:
+        url = self._endpoint("/api/v1/instance")
+        async with self._throttler:
+            async with self._session.get(url=url) as response:
                 if "X-RateLimit-Limit" in response.headers:
                     rate_limit = int(response.headers["X-RateLimit-Limit"])
-                    self.throttler.rate_limit = rate_limit
+                    self._throttler.rate_limit = rate_limit
                 if response.ok:
                     data = await response.text(encoding="utf-8")
                     info = json.loads(data)
                     self.user_count = int(info["stats"]["user_count"])
                 else:
-                    self.handle_error(response)
+                    self._handle_error(response)
 
-    async def get_directory(self, offset, maxsize=80):
+    async def _get_directory(self, offset, maxsize=80):
         """
         https://docs.joinmastodon.org/methods/directory/#get
         """
         if self.debug:
             print(f"({self.domain}) get dir {offset}")
-        url = self.endpoint("/api/v1/directory")
+        url = self._endpoint("/api/v1/directory")
         params = {"offset": offset, "limit": maxsize, "order": "new", "local": "true"}
-        async with self.throttler:
-            async with self.session.get(url=url, params=params) as response:
+        async with self._throttler:
+            async with self._session.get(url=url, params=params) as response:
                 if response.ok:
                     data = await response.text(encoding="utf-8")
                     users = json.loads(data)
@@ -93,9 +92,9 @@ class Instance:
                                 keys = ["id", "followers_count", "following_count"]
                                 self.users[username] = {k: user[k] for k in keys}
                 else:
-                    self.handle_error(response)
+                    self._handle_error(response)
 
-    async def get_following(self, username, url=None, maxsize=80):
+    async def _get_following(self, username, url=None, maxsize=80):
         """
         https://docs.joinmastodon.org/methods/accounts/#following
         """
@@ -103,10 +102,10 @@ class Instance:
             print(f"({self.domain}) get user {username}")
         user_id = self.users[username]["id"]
         if url is None:
-            url = self.endpoint("/api/v1/accounts/" + user_id + "/following")
+            url = self._endpoint("/api/v1/accounts/" + user_id + "/following")
         params = {"limit": maxsize}
-        async with self.throttler:
-            async with self.session.get(url=url, params=params) as response:
+        async with self._throttler:
+            async with self._session.get(url=url, params=params) as response:
                 if response.ok:
                     data = await response.text(encoding="utf-8")
                     following = json.loads(data)
@@ -121,25 +120,29 @@ class Instance:
                         if len(following) == maxsize:
                             pass
                 else:
-                    self.handle_error(response)
+                    self._handle_error(response)
 
-    async def collect(self):
-        self.init_session()
-        await self.get_info()
-        if self.debug:
-            print(
-                f"{self.domain}: {self.user_count} users, rate={self.throttler.rate_limit}"
-            )
+    async def fetch_users(self):
+        self._init_session()
+        await self._get_info()
         maxsize = 80
         await gather(
             *[
-                self.get_directory(offset, maxsize)
+                self._get_directory(offset, maxsize)
                 for offset in range(0, self.user_count, maxsize)
             ]
         )
-        if self.build_graph:
-            usernames = [u for u, v in self.users.items() if v["following_count"] > 0]
+        await self._close_session()
+
+    async def build_graph(self):
+        self._init_session()
+        if self.users:
+            usernames = [
+                u
+                for u, v in self.users.items()
+                if v["following_count"] > 0 and "following" not in v
+            ]
             for username in usernames:
                 self.users[username]["following"] = []
-            await gather(*[self.get_following(username) for username in usernames])
-        await self.close_session()
+            await gather(*[self._get_following(username) for username in usernames])
+        await self._close_session()
