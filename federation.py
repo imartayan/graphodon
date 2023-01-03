64fc3706f2f53b1abff7b1698930d46ac5dc8da8
Igor Martayan <igor@martayan.org>
2023-01-03 22:40:38 +0100

    Load domains before fetching

--- federation.py
+++ federation.py
@@ -9,16 +9,16 @@ from instance import Instance
 class Federation:
     def __init__(self, cache=None, debug=False):
         if cache:
-            self.cache = Path.cwd().joinpath(cache)
-            self.cache.mkdir(parents=True, exist_ok=True)
+            self._cache = Path.cwd().joinpath(cache)
+            self._cache.mkdir(parents=True, exist_ok=True)
         else:
-            self.cache = None
-        self.debug = debug
-        self.catalog = {}
+            self._cache = None
+        self._debug = debug
+        self.domains = {}
         self.instances = {}
-        self.tasks = []
+        self._tasks = []
 
-    async def load_instance_list(self, filename=None):
+    async def load_domains(self, filename=None):
         if filename:
             with open(filename, mode="r") as file:
                 data = file.read()
@@ -40,24 +40,24 @@ class Federation:
             if not isinstance(user_count, int):
                 continue
             if 10 <= user_count <= 20:
-                self.catalog[domain] = user_count
-        if self.debug:
-            print(f"{len(self.catalog)} valid domains")
+                self.domains[domain] = user_count
+        if self._debug:
+            print(f"{len(self.domains)} domains")
 
     async def _init_instance(self, semaphore, domain):
-        if self.cache:
-            file = self.cache.joinpath(domain + ".json")
+        if self._cache:
+            file = self._cache.joinpath(domain + ".json")
             if file.exists():
-                if self.debug:
-                    print(f"{domain} cached")
+                if self._debug:
+                    print(f"{domain} in cache")
                 async with semaphore:
                     self.instances[domain] = await Instance.from_json(file)
                 return
-        self.instances[domain] = Instance(domain, debug=self.debug)
-        self.tasks.append(domain)
+        self.instances[domain] = Instance(domain, debug=self._debug)
+        self._tasks.append(domain)
 
-    async def _init_instances(self, domains, maxopen=200):
-        semaphore = Semaphore(maxopen)
+    async def _init_instances(self, domains, max_open=200):
+        semaphore = Semaphore(max_open)
         await gather(*[self._init_instance(semaphore, domain) for domain in domains])
 
     async def _fetch_instance(self, session, domain):
@@ -67,9 +67,9 @@ class Federation:
             await instance.fetch_users(create_session=False)
             if instance.users:
                 await instance.build_graph(create_session=True)
-                if self.cache:
-                    await instance.export_json(self.cache.joinpath(domain + ".json"))
-            if self.debug:
+                if self._cache:
+                    await instance.export_json(self._cache.joinpath(domain + ".json"))
+            if self._debug:
                 print(f"{domain} fetched")
         except Exception:
             pass
@@ -84,12 +84,14 @@ class Federation:
     def _process_instances(self, domains):
         run(self._fetch_many_instances(domains))
 
-    def fetch_instances(self):
-        run(self._init_instances(self.catalog.keys()))
+    def fetch_all(self):
+        if not self.domains:
+            run(self.load_domains())
+        run(self._init_instances(self.domains.keys()))
         cpus = cpu_count()
         with Pool(processes=cpus) as pool:
-            N = len(self.tasks)
+            N = len(self._tasks)
             buckets = [
-                self.tasks[i * N // cpus : (i + 1) * N // cpus] for i in range(cpus)
+                self._tasks[i * N // cpus : (i + 1) * N // cpus] for i in range(cpus)
             ]
             pool.map(self._process_instances, buckets)
